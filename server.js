89e4bada065a6daac0c1dfd925d587a6f6263aa9
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2017-03-04 12:12:42 +0100

    Prueba de modificación de lanzamiento de servidor en Heroku.

--- server.js
+++ server.js
@@ -1,30 +1,41 @@
-var express, http, sysPath, statik;
+var express, http, sysPath;
 express = require('express');
 sysPath = require('path');
 http = require('http');
 exports.startServer = function(port, path, callback) {
     var app, server;
     app = express();
+
+    console.log("Ruta configurada en app:", path);
+
     app.use(express.static(path));
 
     require('./express/router')(app);
 
     app.all("/", function(req, res) {
-        return res.redirect('/index.html');
+        console.log("Entramos en ruta: /");
+        console.log("Ruta recibida:", req.url);
+        return res.redirect('index.html');
     });
 
     app.get("/docs*", function(req, res) {
         return res.sendFile(sysPath.resolve(sysPath.join(path, "docs/index.html")));
     });
 
-    app.all('/*', function(request, response) {
-        return response.sendFile(sysPath.resolve(sysPath.join(path, 'index.html')));
+    app.all('/*', function(req, res) {
+        console.log("Entramos en ruta: /*");
+        console.log("Ruta recibida:", req.url);
+
+        var filePath = sysPath.resolve(sysPath.join(path, 'index.html'));
+
+        console.log("Ubicacion del fichero a retornar:", filePath);
+        return res.sendFile(filePath);
     });
 
     server = http.createServer(app);
 
     // Inicializacion alternativa en Heroku
-    port = port || 3333;
+    port = process.env.port || port;
 
     server.listen(parseInt(port, 10), callback);
 
@@ -36,7 +47,11 @@ exports.startServer = function(port, path, callback) {
 var portHeroku = process.env.port;
 
 if(portHeroku){
-    exports.startServer(portHeroku, './public', function(err, res){
+    /*exports.startServer(portHeroku, './public', function(err, res){
         console.log("Servidor arrancado en Heroku correctamente en puerto:", portHeroku);
-    });
+    });*/
+
+    console.log("Vamos a lanzar el servidor en Heroku");
+
+    exports.startServer();
 }
